0a7c7d522600495184dd1197b1cb9e1e0ef5824d
erikseyti <erikseyti@gmail.com>
2017-08-25 14:01:54 -0300

    adiconado a funcionalidade de busca do codigo ibge da cidade, informando seu estado

--- app/nfe/controllers.py
+++ app/nfe/controllers.py
@@ -42,9 +42,71 @@ def status():
 def _cod_ibge():
 	import os
 	import pynfe
+	cd = "50"
+	if request.method == "POST":
+		uf = request.form["uf"]
+		if uf == "RR":
+			cd = "14"
+		elif uf == "PR":
+			cd = "41"
+		elif uf == "CE":
+			cd = "23"
+		elif uf == "RS":
+			cd = "43"
+		elif uf == "PI":		
+			cd = "22"
+		elif uf == "GO":
+			cd = "52"
+		elif uf == "RO":
+			cd = "11"
+		elif uf == "PE":
+			cd = "26"
+		elif uf == "MT":
+			cd = "51"
+		elif uf == "MG":
+			cd = "31"
+		elif uf == "SC":
+			cd = "42"
+		elif uf == "BA":
+			cd = "29"
+		elif uf == "RJ":
+			cd = "33"
+		elif uf == "AP":
+			cd = "16"
+		elif uf == "SE":
+			cd = "28"
+		elif uf == "AM":
+			cd = "13"
+		elif uf == "MS":
+			cd = "50"
+		elif uf == "SP":
+			cd = "35"
+		elif uf == "DF":
+			cd = "53"
+		elif uf == "PB":
+			cd = "25"
+		elif uf == "AL":
+			cd = "27"
+		elif uf == "RN":
+			cd = "24"
+		elif uf == 	"MA":
+			cd = "21"	
+		elif uf == "PA":
+			cd = "15"
+		elif uf == "TO":
+			cd = "17"
+		elif uf ==  "AC":
+			cd = "12"
+		elif uf == "ES":
+			cd = "32"
+
+
+	print(uf)
+	print(cd)
 	# 'MunIBGE-UF'+dic[uf]+'.txt'
-	dic = {'PR':'41'}
-	arquivo = os.path.dirname(pynfe.__file__)+'/data/MunIBGE/MunIBGE-UF'+dic['PR']+'.txt'
+
+	dic = {uf:cd}
+	arquivo = os.path.dirname(pynfe.__file__)+'/data/MunIBGE/MunIBGE-UF'+dic[uf]+'.txt'
 	with open(arquivo, 'r') as file:
 		m = file.read()
 	msg = {"value": m, "status":"OK"}
